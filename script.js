3714915a27071c9c0a7a8283770a48d9722eb293
Warunee Dinunzio <dinunziow@gmail.com>
2021-09-24 21:02:58 -0400

    more practice

---
+++ script.js
@@ -0,0 +1,37 @@
+// Mix Mix Mix
+// Create a function called mixUp that takes in two arguments that are strings. Inside the function return the two strings using concat() separated by a space.
+// Using the slice() method and swapping the first 2 characters of each.
+// Replace the second character of each argument with any letter that you want.
+// Each string argument must be three characters or more. If not, return "invalid strings"
+
+
+// Example:
+/* 
+  mixUp('titus', 'rainey') => 'tatus roiney'
+  mixUp('sad', 'boy') => 'sud bey'
+*/
+
+// Even sequence
+// Write a function that returns a sequence with the index beginning at 1 of all the even characters from a string.
+// If the argument string is less than 2 characters or longer than 20 characters, return "invalid string"
+
+// Example:
+/* 
+  evenSequence("abcdefghi") => ["b", "d", "f", "h"]
+
+*/
+
+// ========== Hacker Rank ==========
+//Easy-Rated Hacker Rank Problems:
+https://www.hackerrank.com/challenges/camelcase/problem
+https://www.hackerrank.com/challenges/caesar-cipher-1/problem
+https://www.hackerrank.com/challenges/palindrome-index/problem
+https://www.hackerrank.com/challenges/anagram/problem
+
+/* Medium-Rated HackerRank Problems:
+https://www.hackerrank.com/challenges/sherlock-and-anagrams/problem
+https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem
+
+Hard-Rated HackerRank Problem:
+https://www.hackerrank.com/challenges/challenging-palindromes/problem
+ */
